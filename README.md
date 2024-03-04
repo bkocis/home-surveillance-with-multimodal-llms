8e74d5f5ab67d9942c119b43a626baa2223217a8
Balazs.Kocsis <balaz.kocis@gmail.com>
2024-03-05 00:12:53 +0100

    temperature reduction | readme added

---
+++ README.md
@@ -0,0 +1,48 @@
+
+---
+
+## Home surveillance with Ollama
+
+__Can LLMs be used to analyze video feed?__
+
+#### Description
+
+A video stream from a web-camera is fed into a multimodal LLM. Every nth frame is then captioned. Additional questions can be 
+added, to get more textual description about an image. For this case it is assumed that the web camera is stationary and 
+has the purpose to observe a scene. 
+
+Ollama offers a platform for hosting Large Language Models on your own hardware. You can choose from ollama's ever-growing
+model library of latest LLMs. 
+
+One of the models is LlaVa (Large language Visual Assistant). LLaVa is a multimodal model, which allows the user to
+caption and understand images. Given an image it can describe it, extract information, and on top of that - to use its
+knowledge and context to "chat with the image".
+
+A quantized versions of LlaVa are readily available. Here we will be using a
+[**4Bit** quantized version of the **Llava 1.6** model](https://ollama.com/library/llava/tags).
+
+#### Installation
+
+For the purpose of this demo, we will use a small (7b) model with 2Bit quantization. 
+After installing `ollama`, the specific model can be downloaded by the following command:
+
+`ollama run llava:7b-v1.6-mistral-q2_K`
+
+List out `ollama` models on your local: 
+
+`ollama list`
+
+Once the model is available, running the python app will load the models in the `ollama.chat` method. 
+
+Additional packages are required, in requirements.txt
+
+#### Running the application
+
+This app demonstrated the applicability of the multimodal LLM for "real time" evaluation of video-feed. The results of 
+the model are in this case just dumped into the std output. 
+Run the application (after setting up a venv and sourcing it):
+
+`python demo_cli_llava.py`
+
+
+
--- demo_cli_llava.py
+++ demo_cli_llava.py
@@ -8,6 +8,9 @@ def query_the_image(query: str, image_list: list[str]) -> ollama.chat:
     try:
         res = ollama.chat(
             model='llava:7b-v1.6-mistral-q2_K',
+            options={
+                'temperature': 0
+            },
             messages=[
                 {
                     'role': 'user',
@@ -22,14 +25,14 @@ def query_the_image(query: str, image_list: list[str]) -> ollama.chat:
     return res['message']['content']
 
 
-def observe_scene_change(initial_scene: str, image):
+def observe_scene_change(initial_scene: str, image) -> None:
     queries = [f"You are a home surveillance system - "
                f"you need to observe the image and report very short way with only yes or no "
                f"if you see something different in the image relative to the initial scene described as "
                f"'{initial_scene}'. "
                f"Don't use any other sentences to describe the image!",
                "report very short way with only yes or no - Do you see people in the image?",
-               "report very short way with only yes or no - Do you see someone steeling something?",
+               "report very short way with only yes or no - Do you see someone stealing something?",
                "report very short way with only yes or no - Do you see people moving?"
                ]
     for guery in queries:
@@ -38,32 +41,31 @@ def observe_scene_change(initial_scene: str, image):
 
 def print_out_the_response(query_message: str, image_list: list[str]) -> None:
     response = query_the_image(query_message, image_list)
-    if response:
-        print(f"{str(datetime.datetime.now())}, {response}")
-        sys.stdout.flush()
-        sys.stdout.write("\n")
-    return response
+    print(f"{str(datetime.datetime.now())}, {response}")
+    sys.stdout.flush()
+    sys.stdout.write("\n")
 
 
-def display_image(cap: cv2.VideoCapture):
+def display_image(cap: cv2.VideoCapture) -> None:
     while True:
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow('Webcam Live', frame)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 
-def image_processing_function(frame):
+def image_processing_function(frame) -> None:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow('Webcam Live', gray)
     image = [cv2.imencode('.jpg', gray)[1].tobytes()]
-    # scene_description = print_out_the_response("what is on the image?", image_list=image)
-    # observe_scene_change(initial_scene=initial_scene, image=image)
+    pass_image_to_llava(image)
+
+
+def pass_image_to_llava(image) -> None:
+    # scene_description = print_out_the_response("What is on the image?", image_list=image)
+    # observe_scene_change(initial_scene=scene_description, image=image)
     print_out_the_response("Is there something dangerous going on in the image? Answer with yes or no!",
                            image_list=image)
 
@@ -78,7 +80,7 @@ def frame_generator(cap: cv2.VideoCapture):
         yield frame_number, frame
 
 
-def display_frame_generator(cap: cv2.VideoCapture, image_processing_function: callable, every_nth_second: int):
+def display_frame_generator(cap: cv2.VideoCapture, image_processing_function: callable, every_nth_second: int) -> None:
     frame_rate = 30
     for frame_number, frame in frame_generator(cap):
         if frame_number % (every_nth_second * frame_rate) == 0:
@@ -90,22 +92,4 @@ if __name__ == "__main__":
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
 
-    # display_image(cap)  # for visual debugging
-
     display_frame_generator(cap, image_processing_function=image_processing_function, every_nth_second=5)
-
-
-"""
-import concurrent.futures
-
-def process_frame(frame):
-    return cv2.imencode('.jpg', frame)[1].tobytes()
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        future = executor.submit(process_frame, frame)
-        image = future.result()
-"""
